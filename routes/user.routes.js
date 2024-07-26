342b3430e99804428b277b7b86c81a0dc15f5215
priya <priyankatathe4@gmail.com>
2024-07-26 16:05:53 +0530

    email cookie

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,7 @@
 const router = require("express").Router()
-const userProtected = require("../middleware/protected")
+const { userProtected } = require("../middleware/protected")
 const userController = require("./../controllers/user.controller")
 router
     .post("/verify-user-email", userProtected, userController.verifyUserEmail)
-
 
 module.exports = router
