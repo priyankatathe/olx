f6041de0c1a11b85141ce5d9a85e84fab2078e7a
priya <priyankatathe4@gmail.com>
2024-07-26 16:28:35 +0530

    email cookie

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,12 @@ exports.verifyUserEmail = asyncHandler(async (req, res) => {
     const result = await User.findById(req.loggedInUser)
     const otp = Math.floor(10000 + Math.random() * 900000)
     await User.findByIdAndUpdate(req.loggedInUser, { emailcode: otp })
-    await sendEmail({ to: result.email, subject: "Verify Email", message: otp })
+
+    const isSend = await sendEmail({
+        to: result.email,
+        subject: "Verify Email",
+        message: `<p>your otp ${otp}</p>`
+    })
 
     res.json({ message: "Verification Send Success" })
 })
