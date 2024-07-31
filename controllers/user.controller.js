150e7f6a76c3e6e6fc3c3205cf3a937a6e881ba7
priya <priyankatathe4@gmail.com>
2024-07-31 14:55:42 +0530

    gps location fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -72,12 +72,19 @@ exports.verifyMobileOTP = asyncHandler(async (req, res) => {
     })
 })
 exports.addPost = asyncHandler(async (req, res) => {
-    const { user, category, title, desc, price, images, location } = req.body
+    const { user, category, title, desc, price, images, location, gps } = req.body
     const { error, isError } = checkEmpty({ title, desc, price, images, location, category })
     if (isError) {
         return res.status(400).json({ message: "All Fields Required", error })
     }
+    if (gps) {
+        //api call to openCageData
+        const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=${process.env.OPEN_CAGE_API_KEY}f&q=${location.latitude}%2C${location.longitude}&pretty=1&no_annotations=1`)
+
+        const x = await response.json()
+        console.log(x)
+    }
     // modify this code to support cloudnary
-    await Posts.create({ title, category, desc, price, images, location, user: req.loggedInUser })
+    // await Posts.create({ title, category, desc, price, images, location, user: req.loggedInUser })
     res.json({ message: "Post create success" })
 })
